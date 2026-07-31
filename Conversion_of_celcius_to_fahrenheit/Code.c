9f5765cde52b270f0fa0f5208b84cc60f758c724
leela-ECE-vlsi <leela2002.ece@gmail.com>
2026-07-31 16:02:05 +0530

    Create Code.c

---
+++ Conversion_of_celcius_to_fahrenheit/Code.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+float convert temp(float celsius);
+int main(){
+  float far=convert Temp(32);
+printf("far:%f",far);return 0;
+}
+float convert Temp(float celsius){
+float far = celsius*(9.0/5.0)+32;
+return far;
+}
